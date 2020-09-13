4454506aa754022560e6bd272d82161db6980ebb
TJFitz <tjfitz@comcast.net>
2020-09-13 11:58:36 -0400

    heroku issue

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,16 @@
 const mysql = require("mysql");
-
-const conn = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "root",
-  database: "burgers_db",
-});
+let conn;
+if (process.env.JAWSDB_URL) {
+  conn = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  conn = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "root",
+    database: "burgers_db",
+  });
+}
 
 conn.connect((err) => {
   if (err) {
